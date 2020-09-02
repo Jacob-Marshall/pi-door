fcea2d0e3f60963f736e64c732ea9cce20ae9dab
Jacob Marshall <Jacob-Marshall@users.noreply.github.com>
2020-09-02 16:19:39 +0100

    Update main.py

--- main.py
+++ main.py
@@ -7,8 +7,6 @@ door = Button(26)
 now = datetime.now()
 current_time = now.strftime("%H:%M:%S")
 
-print("It's working!")
-
 while True:
     if door.is_pressed:
         print("Your door was opened at", current_time)
