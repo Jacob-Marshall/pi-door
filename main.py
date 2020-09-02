181677769790d7a73036c98a75dd2a8c2af0e278
Jacob Marshall <Jacob-Marshall@users.noreply.github.com>
2020-09-02 16:30:22 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,14 +3,26 @@ from gpiozero import Button
 from time import sleep
 from datetime import datetime
 
+# Variables
 door = Button(26)
 now = datetime.now()
 current_time = now.strftime("%H:%M:%S")
+door_open_time = ""
+
+# File handling
+f = open("log.txt", "w")
+f = open("log.txt", "a")
+f.write(door_open_time)
+f.close()
+
+#open and read the file after the appending:
+f = open("log.txt", "r")
+print(f.read()) 
 
 print("It's working!")
 
 while True:
     if door.is_pressed:
-        print("Your door was opened at", current_time)
+        door_open_time = "Your door was opened at", current_time
     sleep(1)
     
